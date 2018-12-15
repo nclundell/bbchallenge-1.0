cc84519bb2c93c786af538dbbac69ad89d1cf9fa
Nathan Lundell <nclundell@gmail.com>
2018-12-15 12:04:11 -0500

    Remove date restriction

--- home.php
+++ home.php
@@ -21,51 +21,47 @@
 		</div>
     <div class="container-fluid">
       <?php
-        $date = date("n/j");
-        $day = date("z");
-        if(($day >= 349) || ($day <= 8)){
-          echo "<table class='table table-hover table-striped table-fixed'>";
-            //Generate Table
-            $data = file("data.csv");
-            $dataArray = [];
-            for($i=0; $i<count($data); $i++){
-              array_push($dataArray, explode(",",$data[$i]));
+        echo "<table class='table table-hover table-striped table-fixed'>";
+          //Generate Table
+          $data = file("data.csv");
+          $dataArray = [];
+          for($i=0; $i<count($data); $i++){
+            array_push($dataArray, explode(",",$data[$i]));
+          }
+          //Head
+          echo "<thead><tr>";
+            //echo "<th class='date'>".$dataArray[0][0]."</th>";
+            echo "<th class='time'>".$dataArray[0][1]."</th>";
+            echo "<th class='tv'>".$dataArray[0][2]."</th>";
+            echo "<th class='bowl'>".$dataArray[0][3]."</th>";
+            for($i=6; $i<count($dataArray[0])-1; $i++){
+              echo "<th class='teamname'>".$dataArray[0][$i]."</th>";
             }
-            //Head
-            echo "<thead><tr>";
-              //echo "<th class='date'>".$dataArray[0][0]."</th>";
-              echo "<th class='time'>".$dataArray[0][1]."</th>";
-              echo "<th class='tv'>".$dataArray[0][2]."</th>";
-              echo "<th class='bowl'>".$dataArray[0][3]."</th>";
-              for($i=6; $i<count($dataArray[0])-1; $i++){
-                echo "<th class='teamname'>".$dataArray[0][$i]."</th>";
+          echo "</tr></thead>";
+            //Body
+          echo "<tbody>";
+            for($i=1; $i<count($dataArray); $i++){
+              if($dataArray[$i][0] == $date){
+                echo "<tr>";
+                  //echo "<td class='date'>".$dataArray[$i][0]."</td>";
+                  echo "<td class='time'>".$dataArray[$i][1]."</td>";
+                  echo "<td class='tv'>".$dataArray[$i][2]."</td>";
+                  echo "<td class='bowl'>".$dataArray[$i][3]."</td>";
+                  echo "<td class='teamname'>"; if(empty($dataArray[$i][6])){echo "-";}else{echo $dataArray[$i][6];} echo "</td>";
+                  echo "<td class='teamname'>"; if(empty($dataArray[$i][7])){echo "-";}else{echo $dataArray[$i][7];} echo "</td>";
+                  echo "<td class='teamname'>"; if($dataArray[$i][8]==1){echo $dataArray[$i][6];}elseif($dataArray[$i][8]==2){echo $dataArray[$i][7];}else{echo "-";} echo "</td>";
+                  for($j=9; $j<count($dataArray[$i])-1; $j++){
+                    echo "<td class='teamname'>";
+                      if($dataArray[$i][$j] == $dataArray[$i][8] && is_numeric($dataArray[$i][8])){echo "&#x2714; ";}
+                      elseif($dataArray[$i][$j] != $dataArray[$i][8] && is_numeric($dataArray[$i][8])){echo "&#x2718; ";}
+                      if($dataArray[$i][$j]==1){echo $dataArray[$i][6];}elseif($dataArray[$i][$j]==2){echo $dataArray[$i][7];}else{echo "-";}
+                    echo "</td>";
+                  }
+                echo "</tr>";
               }
-            echo "</tr></thead>";
-              //Body
-            echo "<tbody>";
-              for($i=1; $i<count($dataArray); $i++){
-                if($dataArray[$i][0] == $date){
-                  echo "<tr>";
-                    //echo "<td class='date'>".$dataArray[$i][0]."</td>";
-                    echo "<td class='time'>".$dataArray[$i][1]."</td>";
-                    echo "<td class='tv'>".$dataArray[$i][2]."</td>";
-                    echo "<td class='bowl'>".$dataArray[$i][3]."</td>";
-                    echo "<td class='teamname'>"; if(empty($dataArray[$i][6])){echo "-";}else{echo $dataArray[$i][6];} echo "</td>";
-                    echo "<td class='teamname'>"; if(empty($dataArray[$i][7])){echo "-";}else{echo $dataArray[$i][7];} echo "</td>";
-                    echo "<td class='teamname'>"; if($dataArray[$i][8]==1){echo $dataArray[$i][6];}elseif($dataArray[$i][8]==2){echo $dataArray[$i][7];}else{echo "-";} echo "</td>";
-                    for($j=9; $j<count($dataArray[$i])-1; $j++){
-                      echo "<td class='teamname'>";
-                        if($dataArray[$i][$j] == $dataArray[$i][8] && is_numeric($dataArray[$i][8])){echo "&#x2714; ";}
-                        elseif($dataArray[$i][$j] != $dataArray[$i][8] && is_numeric($dataArray[$i][8])){echo "&#x2718; ";}
-                        if($dataArray[$i][$j]==1){echo $dataArray[$i][6];}elseif($dataArray[$i][$j]==2){echo $dataArray[$i][7];}else{echo "-";}
-                      echo "</td>";
-                    }
-                  echo "</tr>";
-                }
-              }
-            echo "</tbody>";
-          echo "</table>";
-        }
+            }
+          echo "</tbody>";
+        echo "</table>";
       ?>
     </div>
   </body>
