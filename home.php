e4da6da6d4edfb086e9ea388edf1937ecc21acef
Nathan Lundell <nclundell@pm.me>
2024-12-21 13:24:24 -0500

    drop time and tv from home page

--- home.php
+++ home.php
@@ -18,14 +18,14 @@
 		<div class="page-header" style="padding-top: 70px;">
 			<h1 class="text-center">Welcome!</h1>
 			<?php
-                $day = date("z");	
-                if(($day >= 349) || ($day <= 8)){
-                echo "<p class='text-center'>Today's bowls are listed below.  All games in EST.  (".date("n/j").")</p>";
-                }
-                else{
-                echo "<p class='text-center'>Please submit your bowl selections by December 16th if you have not already done so.</p>";
-                }
-            ?>
+        $day = date("z");	
+        if(($day >= 349) || ($day <= 8)){
+          echo "<p class='text-center'>Today's bowls are listed below.  All games in EST.  (".date("n/j").")</p>";
+        }
+        else{
+          echo "<p class='text-center'>Please submit your bowl selections by December 16th if you have not already done so.</p>";
+        }
+      ?>
 		</div>
     <div class="container-fluid">
       <?php
@@ -42,8 +42,8 @@
             //Head
             echo "<thead><tr>";
               //echo "<th class='date'>".$dataArray[0][0]."</th>";
-              echo "<th class='time'>".$dataArray[0][1]."</th>";
-              echo "<th class='tv'>".$dataArray[0][2]."</th>";
+              //echo "<th class='time'>".$dataArray[0][1]."</th>";
+              //echo "<th class='tv'>".$dataArray[0][2]."</th>";
               echo "<th class='bowl'>".$dataArray[0][3]."</th>";
               for($i=6; $i<count($dataArray[0])-1; $i++){
                 echo "<th class='teamname'>".$dataArray[0][$i]."</th>";
@@ -55,8 +55,8 @@
                 if($dataArray[$i][0] == $date){
                   echo "<tr>";
                     //echo "<td class='date'>".$dataArray[$i][0]."</td>";
-                    echo "<td class='time'>".$dataArray[$i][1]."</td>";
-                    echo "<td class='tv'>".$dataArray[$i][2]."</td>";
+                    //echo "<td class='time'>".$dataArray[$i][1]."</td>";
+                    //echo "<td class='tv'>".$dataArray[$i][2]."</td>";
                     echo "<td class='bowl'>".$dataArray[$i][3]."</td>";
                     echo "<td class='teamname'>"; if(empty($dataArray[$i][6])){echo "-";}else{echo $dataArray[$i][6];} echo "</td>";
                     echo "<td class='teamname'>"; if(empty($dataArray[$i][7])){echo "-";}else{echo $dataArray[$i][7];} echo "</td>";
